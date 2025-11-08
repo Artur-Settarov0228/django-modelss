d65f6d37e9ab24dd8e0d2dc9ce641dc46dfe45a1
settarovartur <settarovartur2@gamil.com>
2025-11-08 20:34:03 +0500

    init

--- core/settings.py
+++ core/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'app.apps.AppsConfig',
+
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
