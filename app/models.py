886a9a59be0e56c4f8e08de5efd7365cd3f21132
settarovartur <settarovartur2@gamil.com>
2025-11-08 23:12:21 +0500

    init

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    PRIOTRY_CHOICES = [
+        ('L', 'muhum'),
+        ('M', 'urtacha'),
+        ('H', 'shartmas'),
+    ]
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    priority = models.CharField(choices=PRIOTRY_CHOICES, default='M', max_length=1)
+
+
+
+
+    def __str__(self):
+        return f"Task(id={self.id}),name={self.name}"
--- app/views.py
+++ app/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
 
 # Create your views here.
+
+def create_task(request: HttpRequest) -> HttpResponse:
+    if request.method =="GET":
+        return render (request, 'create.html')
+    elif request.method == "POST":
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        priority = request.POST.get('priority')
+        print(name, description, priority)
+        return HttpResponse("Task created successfully")
+    
 
